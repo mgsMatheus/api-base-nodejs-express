3cad1ff9d423f2cb6a254c5fd38f619c04bfd241
mgsMatheus <7kiks7@gmail.com>
2023-05-23 13:12:58 -0300

    Finalização do projeto da API

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,41 +2,98 @@
  * @swagger
  * components:
  *   schemas:
- *     Book:
+ *     RedNotice:
  *       type: object
  *       properties:
  *         id:
+ *           type: int
+ *         forename:
  *           type: string
- *         title:
+ *         date_of_birth:
  *           type: string
- *         description:
+ *         entity_id:
  *           type: string
- *         finished:
- *           type: boolean
+ *         nationalities:
+ *           type: string
+ *         name:
+ *           type: string
+ *         _links:
+ *           type: string
  *
  * @swagger
  * tags:
- *   name: Books
- *   description: API for managing books
+ *   name: FBI & Interpool
+ *   description: API para realizar busca em sistemas de informações de criminosos, utilizando FBI API & Interpool Notices API
  */
 
 // ... suas rotas e controladores ...
 
 /**
  * @swagger
- * /book:
+ * /fetch-and-save:
  *   get:
- *     summary: List all books
- *     tags: [Books]
+ *     summary: Pesquisa na API do FBI e salva no banco de dados.
+ *     tags: [FBI]
  *     responses:
  *       200:
- *         description: List of books
- *         content:
+ *         description: utiliza a a API do FBI com um get puxando os dados dos criminosos relacionados a AML como, terroristas, colarinho branco, cyber criminosos, cei, criminosos contra a inteligencia dos EUA &  e salva no banco de dados.
+ *         content: 
  *           application/json:
  *             schema:
  *               type: array
  *               items:
- *                 $ref: '#/components/schemas/Book'
+ *                 $ref: '#/models/criminalModel'
+ */
+
+/**
+ * @swagger
+ * /criminals:
+ *   get:
+ *     summary: Pesquisa os criminosos procurados do FBI.
+ *     tags: [FBI]
+ *     responses:
+ *       200:
+ *         description: utiliza a a API do FBI com um get puxando os dados dos criminosos relacionados a AML como, terroristas, colarinho branco, cyber criminosos, cei, criminosos contra a inteligencia dos EUA e os mostra para o usuário.
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/models/criminalModel'
+ */
+
+/**
+ * @swagger
+ * /fetch-and-save-notices:
+ *   get:
+ *     summary: Utiliza um get dos dados na API da Interpool e salva no banco de dados.
+ *     tags: [Interpol]
+ *     responses:
+ *       200:
+ *         description: utiliza a a API do FBI com um get puxando os dados dos criminosos relacionados a AML como, terroristas, colarinho branco, cyber criminosos, cei, criminosos contra a inteligencia dos EUA &  e salva no banco de dados.
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/models/criminalModel'
+ */
+
+/**
+ * @swagger
+ * /notices:
+ *   get:
+ *     summary: Pesquisa as noticias com maior gravidade da Interpol.
+ *     tags: [Interpol]
+ *     responses:
+ *       200:
+ *         description: Utiliza a a API de noticias da Interpool com um get puxando os dados dos crimes e criminosos mais procurados. 
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/models/criminalModel'
  */
 
 
